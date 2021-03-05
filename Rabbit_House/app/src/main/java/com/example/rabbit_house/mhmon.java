46adb4aadc2b97e9f2fa4d78a47355c1f4a24e69
aokidai <aokidaisuke91@gmail.com>
2021-03-05 09:30:20 +0700

    update gamen

---
+++ Rabbit_House/app/src/main/java/com/example/rabbit_house/mhmon.java
@@ -0,0 +1,14 @@
+package com.example.rabbit_house;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mhmon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mhmon);
+    }
+}
